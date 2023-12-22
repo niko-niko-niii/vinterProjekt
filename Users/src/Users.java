400c3242ea63bba68dae280ac3d84a0add0b190d
ok? <122454665+niko-niko-niii@users.noreply.github.com>
2023-12-22 12:26:21 +0100

    Update Users.java

--- Users/src/Users.java
+++ Users/src/Users.java
@@ -1,19 +1,44 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.*;
+import java.util.ArrayList;
+
 public class Users implements Serializable{
     String name;
     String password;
     long ID;
-    boolean Is_Admin;
 
+    ArrayList<int> cart;
+
+    private static long New_ID()  {
+
+        long id=0;
+        //read .ser file
+
+
+        id++;
+
+        // save id in .ser
+        return id;
+    }
+
+
+
+    //child factory
     public static Admin createAdmin(String name, String password) {
         long id=Users.New_ID();
 
-        return new Admin(name, password, id,true);
+        return new Admin(name, password, id);
 
     }
 
-    private static long New_ID(){
-    return 0;
+
+    public static  Customer createUser(String name, String password) {
+        long id=Users.New_ID();
+
+        return new Customer(name, password, id);
+
     }
 
 }
