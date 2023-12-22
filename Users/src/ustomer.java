a30b4de95176b1f47d23674135d22a5a91ac7051
ok? <122454665+niko-niko-niii@users.noreply.github.com>
2023-12-22 12:27:10 +0100

    Create ustomer.java

---
+++ Users/src/ustomer.java
@@ -0,0 +1,21 @@
+public class Customer extends Users{
+
+    //ArrayList<int> cart;       reference
+
+    Customer(String name, String password, long id) {
+        this.name=name;
+        this.password=password;
+    }
+
+    void addItemToCart(){
+
+    }
+    void removeItemFromCart(){
+
+    }
+    String GetCart(){
+
+        
+        return "y";
+    }
+}
