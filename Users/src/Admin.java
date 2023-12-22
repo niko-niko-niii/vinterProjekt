26badcc30e00b8b4463014ae715744c82ee8efcf
ok? <122454665+niko-niko-niii@users.noreply.github.com>
2023-12-22 12:26:42 +0100

    Update Admin.java

--- Users/src/Admin.java
+++ Users/src/Admin.java
@@ -1,14 +1,46 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.*;
-
 import java.util.ArrayList;
-public class Admin extends Users implements Serializable{
-    ArrayList <int> Basket;
+
+public class Users implements Serializable{
+    String name;
+    String password;
+    long ID;
+
+    ArrayList<int> cart;
+
+    private static long New_ID()  {
+
+        long id=0;
+        //read .ser file
 
 
-    public Admin(String name, String password, long id, boolean Is_Admin) {
-        this.name=name;
-        this.password=password;
+        id++;
 
+        // save id in .ser
+        return id;
+    }
+
+
+
+    //child factory
+    public static Admin createAdmin(String name, String password) {
+        long id=Users.New_ID();
+
+        return new Admin(name, password, id);
 
     }
+
+
+    public static  Customer createUser(String name, String password) {
+        long id=Users.New_ID();
+
+        return new Customer(name, password, id);
+
+    }
+
 }
+
+
